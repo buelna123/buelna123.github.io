5efdde9049fda45f777abc908538991fe1f3f4b6
buelna123 <14miguel13@gmail.com>
2016-06-11 18:09:14 -0600

    proyecto web
    Miguel Buelna

--- js/main.js
+++ js/main.js
@@ -21,3 +21,17 @@ $(document).ready(function()
         }
       });
 });
+
+$(document).ready(function()
+{
+    $(window).scroll(function()
+    {
+        var barra = $(window).scrollTop();
+        var posicion = barra * 0.10;
+        
+        $('body').css
+        ({
+            'background-position' : '0 -' + posicion + 'px'
+        });
+    });
+});
