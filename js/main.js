c9bbdabc38d2e32795a2dddefe6123009f0c292c
buelna123 <14miguel13@gmail.com>
2016-06-13 10:08:42 -0600

    proyecto web
    Miguel Buelna

--- js/main.js
+++ js/main.js
@@ -26,16 +26,16 @@ $(document).ready(function()
 {   
     $("#mostrar").click(function()
     {
-        $("p#oculto").show(300);
-        $("a#mostrar").hide(300);
-        $("a#ocultar").show(300);
+        $("p#oculto").show(200);
+        $("a#mostrar").hide();
+        $("a#ocultar").show();
     });
     
     $("#ocultar").click(function()
     {
-        $("p#oculto").hide(300);
-        $("a#mostrar").show(300);
-        $("a#ocultar").hide(300);
+        $("p#oculto").hide(200);
+        $("a#mostrar").show();
+        $("a#ocultar").hide();
     });
 });
 
@@ -43,49 +43,50 @@ $(document).ready(function()
 {   
     $("#mostrar2").click(function()
     {
-        $("p#oculto2").show(300);
-        $("a#mostrar2").hide(300);
-        $("a#ocultar2").show(300);
+        $("p#oculto2").show(200);
+        $("a#mostrar2").hide();
+        $("a#ocultar2").show();
     });
     
     $("#ocultar2").click(function()
     {
-        $("p#oculto2").hide(300);
-        $("a#mostrar2").show(300);
-        $("a#ocultar2").hide(300);
+        $("p#oculto2").hide(200);
+        $("a#mostrar2").show();
+        $("a#ocultar2").hide();
     });
 
 $(document).ready(function()
 {   
     $("#mostrar3").click(function()
     {
-        $("p#oculto3").show(300);
-        $("a#mostrar3").hide(300);
-        $("a#ocultar3").show(300);
+        $("p#oculto3").show(200);
+        $("a#mostrar3").hide();
+        $("a#ocultar3").show();
     });
     
     $("#ocultar3").click(function()
     {
-        $("p#oculto3").hide(300);
-        $("a#mostrar3").show(300);
-        $("a#ocultar3").hide(300);
+        $("p#oculto3").hide(200);
+        $("a#mostrar3").show();
+        $("a#ocultar3").hide();
     });
     
 $(document).ready(function()
 {   
     $("#mostrar4").click(function()
     {
-        $("p#oculto4").show(300);
-        $("a#mostrar4").hide(300);
-        $("a#ocultar4").show(300);
+        $("p#oculto4").show(200);
+        $("a#mostrar4").hide();
+        $("a#ocultar4").show();
     });
     
     $("#ocultar4").click(function()
     {
-        $("p#oculto4").hide(300);
-        $("a#mostrar4").show(300);
-        $("a#ocultar4").hide(300);
+        $("p#oculto4").hide(200);
+        $("a#mostrar4").show();
+        $("a#ocultar4").hide();
     });
 });
 });
 });
+
